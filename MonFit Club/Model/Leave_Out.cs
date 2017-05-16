a6cf55ff1271a907580aea1657463f23b2f6a2d3
Andrew-Montana <le09@ukr.net>
2017-05-16 12:35:19 +0300

    Leave_Out Model updated

--- MonFit Club/Model/Leave_Out.cs
+++ MonFit Club/Model/Leave_Out.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonFit_Club.Models
 {
-    class Leave_Out
+    class Leave_Out : INotifyPropertyChanged
     {
+        private int employee_id;
+        private int client_id;
+        private string action_datetime;
+        private string action_type;
+        private string precedent;
+
+        public int Employee_Id
+        {
+            get { return employee_id; }
+            set
+            {
+                employee_id = value;
+                OnPropertyChanged("Employee_Id");
+            }
+        }
+
+        public int Client_Id
+        {
+            get { return client_id; }
+            set
+            {
+                client_id = value;
+                OnPropertyChanged("Client_Id");
+            }
+        }
+
+        public string Action_DateTime
+        {
+            get { return action_datetime; }
+            set
+            {
+                action_datetime = value;
+                OnPropertyChanged("Action_DateTime");
+            }
+        }
+
+        public string Action_Type
+        {
+            get { return action_type; }
+            set
+            {
+                action_type = value;
+                OnPropertyChanged("Action_Type");
+            }
+        }
+
+        public string Precedent
+        {
+            get { return precedent; }
+            set
+            {
+                precedent = value;
+                OnPropertyChanged("Precedent");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        
     }
 }
