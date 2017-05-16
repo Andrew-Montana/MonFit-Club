b1a7af5f89598a319a470d9f1cab6ac1a59cdb7e
Andrew-Montana <le09@ukr.net>
2017-05-16 17:38:41 +0300

    ClientViewModel Constructor is working, Profile data is loading

--- MonFit Club/ViewModel/ClientViewModel.cs
+++ MonFit Club/ViewModel/ClientViewModel.cs
@@ -1,19 +1,57 @@
-﻿using System;
+﻿using MonFit_Club.Models;
+using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MonFit_Club.ViewModel
 {
     class ClientViewModel : INotifyPropertyChanged
     {
         static private int client_id;
+        public ObservableCollection<Client> Person { get; set; }
 
         static public int Client_Id { get { return client_id; } set { client_id = value; } }
 
+
+        public ClientViewModel()
+        {
+            string full_name = "", gender = "", phone_number = "", card_type = "", card_period_begin = "", card_begin_end = "", password = "";
+
+            NpgsqlConnection connect = new NpgsqlConnection() { ConnectionString = DataBase.connect_params };
+
+            string query = string.Format(@"SELECT phone_number, password, gender, full_name, card_type, card_period[1][1] as begin, card_period[2][1] as end FROM client WHERE id = {0};",client_id);
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connect);
+            connect.Open();
+            try {
+                command.ExecuteNonQuery();
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    full_name = reader["full_name"].ToString();
+                    gender = reader["gender"].ToString();
+                    phone_number = reader["phone_number"].ToString();
+                    card_type = reader["card_type"].ToString();
+                    card_period_begin = reader["begin"].ToString();
+                    card_begin_end = reader["end"].ToString();
+                    password = reader["password"].ToString();
+                }
+                Person = new ObservableCollection<Client>
+                {
+                     new Client { Id = client_id, Phone_Number = phone_number, Password = password, Gender = gender, Full_Name = full_name, Card_Type = card_type, Card_Period_Begin = card_period_begin, Card_Period_End = card_begin_end}
+                };
+  
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message.ToString()); }
+            finally { connect.Close(); }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName]string prop = "")
