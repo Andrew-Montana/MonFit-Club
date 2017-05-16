84e0fb0188c880a80c122cc3d999ed161e65c5a9
Andrew-Montana <le09@ukr.net>
2017-05-16 12:24:37 +0300

    Employee Model updated

--- MonFit Club/Model/Employee.cs
+++ MonFit Club/Model/Employee.cs
@@ -1,13 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonFit_Club.Models
 {
-    class Employee
+    class Employee : INotifyPropertyChanged
     {
-        
+        private int id;
+        private string full_name;
+        private string position;
+        private string phone_number;
+        private string experience_start;
+        private string gender;
+        private double salary;
+        private string password;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Full_Name
+        {
+            get { return full_name; }
+            set
+            {
+                full_name = value;
+                OnPropertyChanged("Full_Name");
+            }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                OnPropertyChanged("Position");
+            }
+        }
+
+        public string Phone_Number
+        {
+            get { return phone_number; }
+            set
+            {
+                phone_number = value;
+                OnPropertyChanged("Phone_Number");
+            }
+        }
+
+        public string Experience_Start
+        {
+            get { return experience_start; }
+            set
+            {
+                experience_start = value;
+                OnPropertyChanged("Experience_Start");
+            }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                gender = value;
+                OnPropertyChanged("Gender");
+            }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                salary = value;
+                OnPropertyChanged("Salary");
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set 
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
     }
 }
